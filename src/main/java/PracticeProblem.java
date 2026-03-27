1933a7d6e27d08d44ece0702b41e216dc8d4689d
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-03-27 15:27:20 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,52 @@
-public class PracticeProblem {
+/*
+File: Lesson 3.4 - Nested Ifs
+Author: Stephen Li
+Date Created: Mar 26, 2026
+Date Last Modified: Mar 26, 2026
+*/
 
-	public static void main(String args[]) {
-
+public class PracticeProblem
+{
+	public static void main(String[] args) {
+	    
 	}
-
+	public static int min(int num1, int num2, int num3) {
+	int placeholder = 1;
+	    if (num1 <= num2) {
+	        if (num1 <= num3) {
+	            return num1;
+	        }
+	        else if (num2 <= num3) {
+	            return num2;
+	        }
+	    }
+	    else {
+	        if (num2 <= num3) {
+	            return num2;
+	        }
+	        else if (num3 <= num2) {
+	            return num3;
+	        }
+	    }
+	    return placeholder;
+	}
+	
+	public static boolean isLeapYear(int year) {
+	    if (year % 4 == 0) {
+	        if (year % 100 == 0) {
+	            if (year % 400 == 0) {
+	                return true;
+	            }
+	            else {
+	                return false;
+	            }
+	        }
+	        else {
+	            return true;
+	        }
+	    }
+	    else {
+	        return false;
+	    }
+	}
 }
